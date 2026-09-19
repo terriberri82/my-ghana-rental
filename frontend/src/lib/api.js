e442bb72b8f2fcb88ce9c803e4ff1845e215c06f
TerriAnn Walker <102176320+terriberri82@users.noreply.github.com>
2026-09-19 11:58:27 -0400

    updated api.js file

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function api(path, options = {}) {
-  const response = await fetch(`${BASE_URL}${path}`, {
+  const response = await fetch(`${BASE_URL}/api${path}`, {
     credentials: "include",
     headers: { "Content-Type": "application/json" },
     ...options,
